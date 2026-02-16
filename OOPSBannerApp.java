6c7a08eb3106f0f86e2b870fce4b0d8bf516ac12
KrishnaAgarwaal <krishna@Krishnas-MacBook-Air-5.local>
2026-02-16 23:12:58 +0530

    UC2: Render OOPS banner using ASCII art with print statements

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,6 +1,17 @@
 public class OOPSBannerApp {
 
     public static void main(String[] args) {
-        System.out.println("OOPS");
+
+        // Each letter width = 9 characters
+        // Height = 7 lines
+
+        System.out.println("  *****   " + "  *****   " + " *******  " + "  *****   ");
+        System.out.println(" **   **  " + " **   **  " + " **    ** " + " **   **  ");
+        System.out.println("**     ** " + "**     ** " + " **    ** " + " **       ");
+        System.out.println("**     ** " + "**     ** " + " *******  " + "  *****   ");
+        System.out.println("**     ** " + "**     ** " + " **       " + "       **  ");
+        System.out.println(" **   **  " + " **   **  " + " **       " + " **   **  ");
+        System.out.println("  *****   " + "  *****   " + " **       " + "  *****   ");
+
     }
 }
