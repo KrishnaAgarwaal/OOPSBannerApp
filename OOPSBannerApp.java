ca3058bf2d8fec48beae06f5c238a4d283e40752
KrishnaAgarwaal <krishna@Krishnas-MacBook-Air-5.local>
2026-02-22 20:02:37 +0530

    UC3: Refactor banner rendering using String.join() for improved memory efficiency

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -2,16 +2,53 @@ public class OOPSBannerApp {
 
     public static void main(String[] args) {
 
-        // Each letter width = 9 characters
-        // Height = 7 lines
+        System.out.println(String.join("",
+                "  *****   ",
+                "  *****   ",
+                " *******  ",
+                "  *****   "
+        ));
 
-        System.out.println("  *****   " + "  *****   " + " *******  " + "  *****   ");
-        System.out.println(" **   **  " + " **   **  " + " **    ** " + " **   **  ");
-        System.out.println("**     ** " + "**     ** " + " **    ** " + " **       ");
-        System.out.println("**     ** " + "**     ** " + " *******  " + "  *****   ");
-        System.out.println("**     ** " + "**     ** " + " **       " + "       **  ");
-        System.out.println(" **   **  " + " **   **  " + " **       " + " **   **  ");
-        System.out.println("  *****   " + "  *****   " + " **       " + "  *****   ");
+        System.out.println(String.join("",
+                " **   **  ",
+                " **   **  ",
+                " **    ** ",
+                " **   **  "
+        ));
 
+        System.out.println(String.join("",
+                "**     ** ",
+                "**     ** ",
+                " **    ** ",
+                " **       "
+        ));
+
+        System.out.println(String.join("",
+                "**     ** ",
+                "**     ** ",
+                " *******  ",
+                "  *****   "
+        ));
+
+        System.out.println(String.join("",
+                "**     ** ",
+                "**     ** ",
+                " **       ",
+                "       ** "
+        ));
+
+        System.out.println(String.join("",
+                " **   **  ",
+                " **   **  ",
+                " **       ",
+                " **   **  "
+        ));
+
+        System.out.println(String.join("",
+                "  *****   ",
+                "  *****   ",
+                " **       ",
+                "  *****   "
+        ));
     }
 }
